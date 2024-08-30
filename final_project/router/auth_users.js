6c8ee7ef49d26eae0f5790e5ce3720ea0756ab11
hayden <haydenyau1314@gmail.com>
2024-08-30 13:56:44 -0400

    commit

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,8 +56,17 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
+    const isbn = req.params.isbn + "";
+    const review = req.query.review;
+    const username = req.user.data;
+    const book = books[isbn];
+    if (book) {
+        book.reviews[username] = review;
+        return res.status(200).json(book);
+    }
+    return res.status(404).json({ message: "Invalid ISBN" });
+
 });
 
 module.exports.authenticated = regd_users;
